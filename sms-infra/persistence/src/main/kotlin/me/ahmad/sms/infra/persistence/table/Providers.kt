ae23cc3553c4d9428574ee98b9c7174c31d9c48c
Ahmad <Eahmad.edge@gmail.com>
2021-04-29 14:44:13 +0430

    Add some statistics column to Providers table.

--- sms-infra/persistence/src/main/kotlin/me/ahmad/sms/infra/persistence/table/Providers.kt
+++ sms-infra/persistence/src/main/kotlin/me/ahmad/sms/infra/persistence/table/Providers.kt
@@ -4,11 +4,18 @@ import me.ahmad.sms.domain.Provider
 import org.ktorm.database.Database
 import org.ktorm.dsl.QueryRowSet
 import org.ktorm.entity.sequenceOf
+import org.ktorm.schema.double
+import org.ktorm.schema.long
 import org.ktorm.schema.varchar
 
 internal object Providers : RichTable<Provider>("providers") {
     val id = varchar("id").primaryKey().transform(Provider::Id) { it.value }
     val address = varchar("address").transform(Provider::Address) { it.value }
+    val totalCount = long("total_count")
+    val doneCount = long("done_count")
+    val donePercent = double("done_percent")
+    val failedCount = long("failed_count")
+    val failedPercent = double("failed_percent")
 
     override fun doCreateEntity(row: QueryRowSet, withReferences: Boolean) = Provider(
         id = row.column(id),
--- sms-infra/persistence/src/main/resources/db/migration/V1__Create_tables.sql
+++ sms-infra/persistence/src/main/resources/db/migration/V1__Create_tables.sql
@@ -6,8 +6,25 @@ CREATE TABLE receivers
 
 CREATE TABLE providers
 (
-    id      VARCHAR(11) PRIMARY KEY,
-    address VARCHAR NOT NULL
+    id             VARCHAR(11) PRIMARY KEY,
+    address        VARCHAR         NOT NULL,
+    total_count    BIGINT          NOT NULL DEFAULT 0,
+    done_count     BIGINT          NOT NULL DEFAULT 0,
+    done_percent   NUMERIC(100, 2) NOT NULL
+        GENERATED ALWAYS AS (
+            CASE
+                WHEN total_count = 0 THEN 0
+                ELSE (done_count / total_count) * 100
+                END
+            ) STORED,
+    failed_count   BIGINT          NOT NULL DEFAULT 0,
+    failed_percent NUMERIC(100, 2) NOT NULL
+        GENERATED ALWAYS AS (
+            CASE
+                WHEN total_count = 0 THEN 0
+                ELSE (failed_count / total_count) * 100
+                END
+            ) STORED
 );
 
 CREATE TABLE messages
